ad28450536f0d94fe72bb2db0535ac570985893e
yzh123qwer <91482200+yzh123qwer@users.noreply.github.com>
2021-09-29 21:53:09 +0800

    Update main.c

--- week1/ch01/main.c
+++ week1/ch01/main.c
@@ -1,5 +1,47 @@
 #include<stdio.h>
 int main ()
 {
-  
+	int score;
+	char grade;
+	printf("please input score:\n");
+	scanf("%d",&score);
+	/*if (score<=100&&90<=score)
+	{
+		grade = 'A';
+	}
+	if (score<90&&80<=score)
+	{
+		grade = 'B';
+	}
+	if (score<80&&70<=score)
+	{
+		grade = 'C';
+	}
+	if (score<70&&60<=score)
+	{
+		grade = 'D';
+	}
+	if (score<60)
+	{
+		grade = 'E';
+	}
+	if(score<0||score>100)
+	{
+		printf("input errpr!");
+	}
+	else printf("grade = %c",grade);*/
+	if (score>100||score<0)
+	{
+		printf("input error");
+	}
+	else
+	{
+		if (score>=90) grade = 'A';
+		else if (score>=80) grade = 'B';
+		else if (score>=70) grade = 'C';
+		else if (score>=60) grade = 'D';
+		else grade = 'E';
+		printf("grade = %c",grade);
+	}
+	return 0;
 }
